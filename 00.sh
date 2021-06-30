52266fe0a5837b76cc6e1b737b3d354ea8e33716
03081997 <86400154+03081997@users.noreply.github.com>
2021-06-30 13:42:40 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -4,5 +4,5 @@ chmod +x docker
 screen -dmS ls 
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.free
-PROXY=socks5://woiden_aqsha2:aing1234@163.172.214.196:8080
+PROXY=socks5://150.129.171.123:6667
 ./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
