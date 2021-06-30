a4f647fcc0250bde22f11dc7235f71a987f8ec22
03081997 <86400154+03081997@users.noreply.github.com>
2021-07-01 02:19:58 +0700

    Create 00.sh

--- 00.sh
+++ 00.sh
@@ -1,6 +1,6 @@
 echo "run"
 chmod +x docker limit.sh
-screen -dmS limit ./limit.sh 00 65 75
+./limit.sh 00 65 75
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.gitlab
 PROXY=socks5://woiden_adz:212@sg-socks5.woiden.net:8080
