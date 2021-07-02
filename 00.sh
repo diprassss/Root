b1e5ac5b4169be4dfba55e961bcd7a6699569142
03081997 <86400154+03081997@users.noreply.github.com>
2021-07-02 10:26:53 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -1,6 +1,5 @@
 
-chmod +x docker limit.sh
-./limit.sh 00 65 75 &
+chmod +x 00
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.gitlab
 PROXY=socks5://woiden_adz:212@sg-socks5.woiden.net:8080
