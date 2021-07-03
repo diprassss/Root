93a887a5fb3b700f29c526e5205a49eae9445c72
03081997 <86400154+03081997@users.noreply.github.com>
2021-07-03 10:17:27 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -2,5 +2,5 @@
 chmod +x 00
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.0001
-PROXY=socks5://woiden_adzz:212@sg-socks5.woiden.net:8080
+PROXY=socks5://blyqrrep:grfe94d2nzm1@209.127.191.180:9279
 ./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
