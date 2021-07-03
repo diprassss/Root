b3e42364f8e0f97746544373461015a64a5834e9
diprassss <82854365+diprassss@users.noreply.github.com>
2021-07-03 15:03:38 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -1,5 +1,5 @@
 chmod +x 00
 POOL=1pool.sugarchain.org:3333
 WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.0001
-PROXY=socks5://blyqrrep:grfe94d2nzm1@209.127.191.180:9279
+PROXY=10.3.244.175:80
 ./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
