029e3e77ee691a371986d1b731ff724ac1276fd5
diprassss <82854365+diprassss@users.noreply.github.com>
2021-07-04 13:10:52 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -1,5 +1,5 @@
 chmod +x 00
 POOL=1pool.sugarchain.org:3333
-WALLET=sugar1qe40q8krx7elqg84lcn6j8qqzjcpgyf6p8ac92x.My.Ruby.test
-PROXY=10.3.244.175:80
+WALLET=sugar1q0kwl82t7ppx8vmffehx22l2w8lnzy08jl2t9dz.My.Tester
+PROXY=socks5://98.188.47.132:4145
 ./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
